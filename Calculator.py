1b77600b306330d21b0fd05acf3b4f809bf8feed
CaCO3ua <caco3rv@gmail.com>
2016-06-16 19:08:48 +0300

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,42 @@
+while 1:
+    try:
+        a = float(input("Type in the first number, please:\n"))
+        break
+    except ValueError:
+        print ("Man, that is not a number, c'mon!")
+
+while 1:
+    try:
+        b = float(input("Type in the second number, please:\n"))
+        break
+    except ValueError:
+        print ("Man, that is not a number, c'mon!")
+
+while 1:
+    operation = (input("Type in the operation, please (+, -, *, /):\n"))
+    if operation == "+" or operation == "-" or operation == "*" or operation == "/":    break
+    else:
+        print ("You can only put +, -, * or /")        
+
+def add(a, b):
+    return a + b
+    
+def multiply(a, b):
+    return a - b
+    
+def subtract(a, b):
+    return a - b
+    
+def divide(a, b):
+    return a / b
+    
+action = {
+	"+" : add,
+	"*" : multiply,
+	"-" : subtract,
+	"/" : divide
+	}
+	
+result = action [operation](a,b)
+
+print (result)
